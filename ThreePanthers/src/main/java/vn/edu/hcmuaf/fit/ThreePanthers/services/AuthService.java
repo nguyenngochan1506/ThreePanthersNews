331187069139a508d948454ff92712341a75dcf1
Meo Han <22130161@st.hcmuaf.edu.vn>
2026-01-05 22:59:37 +0700

    xoá file thừa

--- ThreePanthers/src/main/java/vn/edu/hcmuaf/fit/ThreePanthers/services/AuthService.java
+++ ThreePanthers/src/main/java/vn/edu/hcmuaf/fit/ThreePanthers/services/AuthService.java
@@ -46,8 +46,6 @@ public class AuthService {
 
         String code = String.valueOf(new Random().nextInt(900000) + 100000);
 
-        System.out.println("🔥🔥🔥 MÃ XÁC THỰC CỦA TUI (REGISTER): " + code);
-
         user.setVerificationCode(code);
         user.setVerificationExpiration(LocalDateTime.now().plusMinutes(15)); 
 
